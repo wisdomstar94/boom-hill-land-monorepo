9bd3ac7bb9b6c04c22448d49d5148434a64ffb43
wisdomstar94 <wisdomstar94@gmail.com>
2026-06-14 22:42:58 +0900

    github-pages deploy 코드 작성.

--- apps/boom-hill-land-web/next.config.ts
+++ apps/boom-hill-land-web/next.config.ts
@@ -2,10 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   images: {
     unoptimized: true,
   },
   allowedDevOrigins: ["192.168.0.94"],
+  trailingSlash: true,
 };
 
 export default nextConfig;
