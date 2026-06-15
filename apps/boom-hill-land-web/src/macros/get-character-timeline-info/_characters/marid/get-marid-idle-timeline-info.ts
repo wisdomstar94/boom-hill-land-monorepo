061f363b8c1c9edbd130385e56aac4f5ae59c6bb
wisdomstar94 <wisdomstar94@gmail.com>
2026-06-15 22:17:20 +0900

    에띠, 마리드, 케피 이미지 적용.

--- apps/boom-hill-land-web/src/macros/get-character-timeline-info/_characters/marid/get-marid-idle-timeline-info.ts
+++ apps/boom-hill-land-web/src/macros/get-character-timeline-info/_characters/marid/get-marid-idle-timeline-info.ts
@@ -7,32 +7,32 @@ export function getMaridIdleTimelineInfo(): GifMakerTimelineInfo {
     timelines: [
       {
         uniqueKey: "001",
-        imageUrl: `${BASE_PATH}/boom-hill-character/dao/idle/idle-001.png`, // 정면 응시하는 모습
+        imageUrl: `${BASE_PATH}/boom-hill-character/marid/idle/idle-001.png`, // 정면 응시하는 모습
         durationMs: 2000,
       },
       {
         uniqueKey: "002",
-        imageUrl: `${BASE_PATH}/boom-hill-character/dao/idle/idle-002.png`, // 고개 위로 올리고 눈 뜬 모습
+        imageUrl: `${BASE_PATH}/boom-hill-character/marid/idle/idle-002.png`, // 고개 위로 올리고 눈 뜬 모습
         durationMs: 300,
       },
       {
         uniqueKey: "003",
-        imageUrl: `${BASE_PATH}/boom-hill-character/dao/idle/idle-003.png`, // 고개 위로 올리고 눈 감은 모습
+        imageUrl: `${BASE_PATH}/boom-hill-character/marid/idle/idle-003.png`, // 고개 위로 올리고 눈 감은 모습
         durationMs: 150,
       },
       {
         uniqueKey: "004",
-        imageUrl: `${BASE_PATH}/boom-hill-character/dao/idle/idle-002.png`, // 고개 위로 올리고 눈 뜬 모습
+        imageUrl: `${BASE_PATH}/boom-hill-character/marid/idle/idle-002.png`, // 고개 위로 올리고 눈 뜬 모습
         durationMs: 150,
       },
       {
         uniqueKey: "005",
-        imageUrl: `${BASE_PATH}/boom-hill-character/dao/idle/idle-003.png`, // 고개 위로 올리고 눈 감은 모습
+        imageUrl: `${BASE_PATH}/boom-hill-character/marid/idle/idle-003.png`, // 고개 위로 올리고 눈 감은 모습
         durationMs: 150,
       },
       {
         uniqueKey: "006",
-        imageUrl: `${BASE_PATH}/boom-hill-character/dao/idle/idle-002.png`, // 고개 위로 올리고 눈 뜬 모습
+        imageUrl: `${BASE_PATH}/boom-hill-character/marid/idle/idle-002.png`, // 고개 위로 올리고 눈 뜬 모습
         durationMs: 300,
       },
     ],
